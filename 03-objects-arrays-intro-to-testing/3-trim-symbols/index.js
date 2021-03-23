acbd7edd319cc6404a19d56403fb8f92350c5008
bogota-bathos-cathode <agohado.eonapa@gmail.com>
2021-03-23 16:26:39 +1000

    done: 03.3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let counter = 0;
 
+  return string
+    .split('')
+    .reduce((newString, letter) => {
+      if (newString.slice(-1) === letter) {
+        counter++;
+        if (!size || counter < size) {
+          return newString + letter;
+        }
+        return newString;
+      }
+      counter = 0;
+      return newString + (size !== 0 ? letter : '');
+    }, '');
 }
